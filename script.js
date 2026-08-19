bfa05f7c059de0237e3fdfbcb4782fc5e96806b5
modi1401faisal-svg <modi1401faisal@gmail.com>
2026-08-19 12:33:10 +0300

    creat.script.js

--- script.js
+++ script.js
@@ -11,6 +11,7 @@
    تحميل البيانات
 
 ========================= */
+
 const SUPABASE_URL = "https://uuhldvdgyyxvtmjqqwex.supabase.co";
 
 const SUPABASE_ANON_KEY = "sb_publishable_HM4vP8LSEZJaZC9Cyug5fg_fhQ05QqL";
